558f7a5785acea53b3544d7ebad51b7777edd5a8
LJosueCM <ljosuecm@gmail.com>
2022-04-20 13:01:25 -0500

    Prueba Spiderman fallida

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,10 @@
-describe("Test Suite Dummy Description", () => {
-    test('Case 1 Dummy', () => {
-      const resultOfSomething = 1 + 2
-      expect(resultOfSomething).toBe(18);
+describe("Unit test for Spiderman class", () => {
+    test('1) Create an spiderman object', () => {
+      const andrrewGarfield = new Spiderman("Spiderman Sony",31,"Andrew Garfield",2,"Sony")
+      expect(andrrewGarfield.name).toBe("Spiderman Sony");
+      expect(andrrewGarfield.age).toBe(31);
+      expect(andrrewGarfield.actor).toBe("Andrew Garfield");
+      expect(andrrewGarfield.movies).toBe(2);
+      expect(andrrewGarfield.studio).toBe("Sony");
     });
-  })
+})
